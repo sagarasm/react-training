b802a7068f6a593906bb8349b5f93bd5f1dabeb5
Anchal Katoch <anchal.katoch@perficient.com>
2025-03-17 15:16:45 +0530

    Adding changes to hook file

--- src/app/hooks/page.jsx
+++ src/app/hooks/page.jsx
@@ -4,14 +4,8 @@ import MyProps from '../components/Props'
 export default function Hooks() {
     return (
         <>
-        <h1>Props in React?</h1>
-        <p>In React, props (short for "properties") are a way to pass data from a parent component to a child component. They allow components to be dynamic and reusable by making them configurable.</p>
-        <ul>
-            <li>Props are read-only (immutable) and cannot be modified by the child component.</li>
-            <li>They are passed as attributes to a component in JSX</li>
-            <li>The child component receives them as an argument in the function....</li>
-        </ul>
-        <MyProps />
+        <h1>I am hooks</h1>
+        
         </>
     );
   }
